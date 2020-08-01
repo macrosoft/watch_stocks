483c78f3428757cb4cc7a3604d251507242386d9
Artyom Zagidulin <p0chta@yandex.ru>
2020-08-01 09:28:45 +0300

    add config file

--- watch_stocks.py
+++ watch_stocks.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import json
+import os
 import requests
 import time
+
+base_dir = os.path.dirname(__file__)
+with open(os.path.join(base_dir, "config.json"), "r") as config_file:
+    config = json.load(config_file)
 
 url = "https://iss.moex.com/iss/engines/currency/markets/selt/securities.json?iss.meta=off&iss.only=securities%2Cmarketdata&securities=USD000000TOD"
 while 1:
