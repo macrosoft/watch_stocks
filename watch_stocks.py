f27578f9046fc40bfd228937ecd1759335f9848b
Artyom Zagidulin <p0chta@yandex.ru>
2020-08-05 19:42:39 +0300

    threading sqlite

--- watch_stocks.py
+++ watch_stocks.py
@@ -6,6 +6,7 @@ import os
 import re
 import requests
 import time
+import threading
 import random
 import sqlite3
 from telegram import ReplyKeyboardMarkup, KeyboardButton
@@ -15,6 +16,15 @@ alarm_txt_prev = ['Хорошая новость!', 'Плохая новость
 emoji = ['\U0001F600', '\U0001F603', '\U0001F604', '\U0001F601', '\U0001F606', '\U0001F605', '\U0001F923', '\U0001F602', '\U0001F642', '\U0001F643', '\U0001F609', '\U0001F60A', '\U0001F607', '\U0001F60B', '\U0001F61B', '\U0001F61C', '\U0001F92A', '\U0001F61D', '\U0001F911', '\U0001F92D', '\U0001F92B', '\U0001F914', '\U0001F922', '\U0001F92E', '\U0001F92E', '\U0001F927', '\U0001F975', '\U0001F976', '\U0001F974', '\U0001F635', '\U0001F92F', '\U0001F973', '\U0001F60E', '\U0001F4A9', '\U0001F648', '\U0001F649', '\U0001F64A', '\U0001F90F', '\U0001F91F', '\U0001F595', '\U0001F44D', '\U0001F44E', '\U0001F4AA', '\U0001F9E8', '\U0001F910','\U0001F928','\U0001F610','\U0001F611','\U0001F636','\U0001F60F','\U0001F612','\U0001F644','\U0001F62C','\U0001F925','\U0001F637','\U0001F44C','\U0001F926']
 USDTOD = 0
 EURTOD = 0
+lock = threading.Lock()
+
+def select(q):
+	try:
+		lock.acquire(True)
+		cursor.execute(q)
+	finally:
+		lock.release()
+	return cursor.fetchall()
 
 def help_command(update, context):
 	update.message.reply_text('Напиши название ценной бумаги для того чтобы найти её.\n/status - показать подписки\n/currency - курс валют')
@@ -33,8 +43,7 @@ def currency_command(update, context):
 	update.message.reply_text(text)
 
 def status_command(update, context):
-	cursor.execute("SELECT t.code, type, refval, t.value FROM subscribe s JOIN ticker t ON t.id = s.ticker WHERE uid = %d ORDER BY class, t.code" % (update.effective_chat.id))
-	rows = cursor.fetchall()
+	rows = select("SELECT t.code, type, refval, t.value FROM subscribe s JOIN ticker t ON t.id = s.ticker WHERE uid = %d ORDER BY class, t.code" % (update.effective_chat.id))
 	text = "Действующих подписок нет \U0001F610"
 	if len(rows) > 0:
 		text = "Твои подписки:\n"
@@ -45,8 +54,7 @@ def status_command(update, context):
 			dirn = "rise" if row[1] else "fall"
 			text += "%s %s: %.4f (%s%.4f)\nОтписаться: /unsub_%s_%s\n" % (dir, row[0], row[3], tdir, t, row[0].lower(), dirn)
 	else:
-		cursor.execute("SELECT code, fullname FROM ticker  WHERE value NOT NULL ORDER BY random() LIMIT 1")
-		rows = cursor.fetchall()
+		rows = select("SELECT code, fullname FROM ticker  WHERE value NOT NULL ORDER BY random() LIMIT 1")
 		for row in rows:
 			text += "\n\nМогу предложить подписаться на:\n"
 			text += "%s %s\n" % (row[1], random.choice(emoji))
@@ -55,15 +63,18 @@ def status_command(update, context):
 
 def subscribe_command(update, context):
 	arg = update.message.text.replace('/sub_', '').split('_')
-	cursor.execute("SELECT id, value FROM ticker WHERE code like '%s'" % (arg[0]))
-	rows = cursor.fetchall()
+	rows = select("SELECT id, value FROM ticker WHERE code like '%s'" % (arg[0]))
 	if len(rows) < 1:
 		return
 	ticker_id = rows[0][0]
 	value = rows[0][1]
 	dir = 1 if arg[1] == 'rise' else 0
-	cursor.execute("INSERT OR REPLACE INTO subscribe (uid, ticker, type, refval, dt) VALUES(%d, %d, %d, (SELECT value FROM ticker WHERE id = %d), DATETIME(CURRENT_TIMESTAMP, 'localtime'))" % (update.effective_chat.id, ticker_id, dir, ticker_id))
-	conn.commit()
+	try:
+		lock.acquire(True)
+		cursor.execute("INSERT OR REPLACE INTO subscribe (uid, ticker, type, refval, dt) VALUES(%d, %d, %d, (SELECT value FROM ticker WHERE id = %d), DATETIME(CURRENT_TIMESTAMP, 'localtime'))" % (update.effective_chat.id, ticker_id, dir, ticker_id))
+		conn.commit()
+	finally:
+		lock.release()
 	dirtxt = "расти" if dir else "падать"
 	dirtrg = ">" if dir else "<"
 	k = 1.0025 if dir else 0.9975
@@ -72,27 +83,29 @@ def subscribe_command(update, context):
 def unsubscribe_command(update, context):
 	arg = update.message.text.replace('/unsub_', '').split('_')
 	emoji = ['\U0001F910','\U0001F928','\U0001F610','\U0001F611','\U0001F636','\U0001F60F','\U0001F612','\U0001F644','\U0001F62C','\U0001F925','\U0001F637','\U0001F44C','\U0001F926']
-	if arg[1] == 'rise':
-		cursor.execute("DELETE FROM subscribe WHERE uid = %d AND ticker = (SELECT id FROM ticker WHERE code like '%s') AND type = 1" % (update.effective_chat.id, arg[0]))
-		conn.commit()
-		update.message.reply_text("Больше ни слова о росте %s %s" % (arg[0].upper(), random.choice(emoji)))
-	if arg[1] == 'fall':
-		cursor.execute("DELETE FROM subscribe WHERE uid = %d AND ticker = (SELECT id FROM ticker WHERE code like '%s') AND type = 0" % (update.effective_chat.id, arg[0]))
-		conn.commit()
-		update.message.reply_text("Больше ни слова о падении %s %s" % (arg[0].upper(), random.choice(emoji)))
+	try:
+		lock.acquire(True)
+		if arg[1] == 'rise':
+			cursor.execute("DELETE FROM subscribe WHERE uid = %d AND ticker = (SELECT id FROM ticker WHERE code like '%s') AND type = 1" % (update.effective_chat.id, arg[0]))
+			conn.commit()
+			update.message.reply_text("Больше ни слова о росте %s %s" % (arg[0].upper(), random.choice(emoji)))
+		if arg[1] == 'fall':
+			cursor.execute("DELETE FROM subscribe WHERE uid = %d AND ticker = (SELECT id FROM ticker WHERE code like '%s') AND type = 0" % (update.effective_chat.id, arg[0]))
+			conn.commit()
+			update.message.reply_text("Больше ни слова о падении %s %s" % (arg[0].upper(), random.choice(emoji)))
+	finally:
+		lock.release()
 
 def show_command(update, context):
 	arg = update.message.text.replace('/show_', '').split('_')
-	cursor.execute("SELECT id, code, fullname, value FROM ticker WHERE code like '%s'" % (arg[0]))
-	rows = cursor.fetchall()
+	rows = select("SELECT id, code, fullname, value FROM ticker WHERE code like '%s'" % (arg[0]))
 	if len(rows) < 1:
 		return
 	row = rows[0]
 	id = row[0]
 	ticker = row[1]
 	text = "%s: %.4f\n%s\n" % (row[1], row[3], row[2])
-	cursor.execute("SELECT t, type FROM (SELECT 1 t UNION SELECT 0) tt LEFT JOIN subscribe s ON s.type = tt.t AND uid = %d AND ticker = %d ORDER BY t" % (update.effective_chat.id, id))
-	rows = cursor.fetchall()
+	rows = select("SELECT t, type FROM (SELECT 1 t UNION SELECT 0) tt LEFT JOIN subscribe s ON s.type = tt.t AND uid = %d AND ticker = %d ORDER BY t" % (update.effective_chat.id, id))
 	for row in rows:
 		if row[0] == 0:
 			if row[1] is None:
@@ -119,8 +132,7 @@ def load_securities_from_moex(url, c):
 	if len(q) > 0:
 		q = "INSERT OR IGNORE INTO ticker (code, shortname, fullname, uc_shortname, uc_fullname, class) VALUES" + q
 		cursor.execute(q)
-	cursor.execute("CREATE TABLE IF NOT EXISTS subscribe (uid INTEGER, ticker INTEGER, type INTEGER, refval REAL, rate REAL, dt DATETIME, UNIQUE(uid, ticker, type))")
-	conn.commit()
+		conn.commit()
 
 def update_prices(url):
 	r = requests.get(url=url)
@@ -128,7 +140,7 @@ def update_prices(url):
 	for row in data['marketdata']['data']:
 		if row[1] is not None:
 			cursor.execute("UPDATE ticker SET value = %f, dt = DATETIME(CURRENT_TIMESTAMP, 'localtime') WHERE code = '%s'" % (row[1], row[0]))
-	conn.commit()
+			conn.commit()
 
 def search(update, context):
 	t = update.message.text
@@ -141,8 +153,12 @@ def search(update, context):
 		return
 	text = ""
 	t = "%%" + t.upper() + "%%"
-	cursor.execute("SELECT code, fullname, value FROM ticker WHERE code like ? OR uc_shortname like ? OR uc_fullname like ? AND value NOT NULL LIMIT 5", (t, t, t))
-	rows = cursor.fetchall()
+	try:
+		lock.acquire(True)
+		cursor.execute("SELECT code, fullname, value FROM ticker WHERE code like ? OR uc_shortname like ? OR uc_fullname like ? AND value NOT NULL LIMIT 5", (t, t, t))
+		rows = cursor.fetchall()
+	finally:
+		lock.release()
 	for row in rows:
 		text += "%s: %.4f\n%s\n" % (row[0], row[2], row[1])
 		text += "/show_%s - подробнее\n\n" % (row[0].lower())
@@ -166,6 +182,8 @@ url = "https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQOB/securiti
 load_securities_from_moex(url, 3)
 url = "https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities.json?iss.meta=off&iss.only=securities&securities.columns=SECID,SHORTNAME,SECNAME"
 load_securities_from_moex(url, 3)
+cursor.execute("CREATE TABLE IF NOT EXISTS subscribe (uid INTEGER, ticker INTEGER, type INTEGER, refval REAL, rate REAL, dt DATETIME, UNIQUE(uid, ticker, type))")
+conn.commit()
 
 updater = Updater(token=config['TOKEN'], use_context=True)
 dp = updater.dispatcher
@@ -186,26 +204,30 @@ url_bonds_TQCB = "https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQ
 while 1:
 	r = requests.get(url=url_currency)
 	data = r.json()
-	for row in data['marketdata']['data']:
-		if row[0] == 'USD000000TOD' and row[1] is not None:
-			USDTOD = row[1]
-			cursor.execute("UPDATE ticker SET value = %f, dt = DATETIME(CURRENT_TIMESTAMP, 'localtime') WHERE code = 'USD'" % (USDTOD))
-		if row[0] == 'EUR_RUB__TOD' and row[1] is not None:
-			EURTOD = row[1]
-			cursor.execute("UPDATE ticker SET value = %f, dt = DATETIME(CURRENT_TIMESTAMP, 'localtime') WHERE code = 'EUR'" % (EURTOD))
-	update_prices(url_stocks)
-	update_prices(url_bonds_TQOB)
-	update_prices(url_bonds_TQCB)
-	cursor.execute("UPDATE subscribe SET refval = (SELECT value FROM ticker WHERE id = subscribe.ticker) WHERE type = 0 AND refval < (SELECT value FROM ticker WHERE id = subscribe.ticker)")
-	cursor.execute("UPDATE subscribe SET refval = (SELECT value FROM ticker WHERE id = subscribe.ticker) WHERE type = 1 AND refval > (SELECT value FROM ticker WHERE id = subscribe.ticker)")
-	conn.commit()
-	cursor.execute("SELECT uid, t.code, type, refval, t.value, ticker FROM subscribe s JOIN ticker t ON t.id = s.ticker WHERE type = 1 AND t.value > refval*1.0025 OR type = 0 AND t.value < refval*0.9975")
-	rows = cursor.fetchall()
-	for row in rows:
-		dir = "вырос \U0001F4C8" if row[2] else "упал \U0001F4C9"
-		dirn = "rise" if row[2] else "fall"
-		text = "%s %s %s с %.4f до %.4f. %s Продолжить наблюдение: /sub_%s_%s" % (random.choice(alarm_txt_prev), row[1], dir, row[3], row[4], random.choice(emoji), row[1].lower(), dirn)
-		updater.bot.send_message(chat_id=row[0], text=text)
-		cursor.execute("DELETE FROM subscribe WHERE uid = %d AND ticker = %d AND type = %d" % (row[0], row[5], row[2]))
-	conn.commit()
+	try:
+		lock.acquire(True)
+		for row in data['marketdata']['data']:
+			if row[0] == 'USD000000TOD' and row[1] is not None:
+				USDTOD = row[1]
+				cursor.execute("UPDATE ticker SET value = %f, dt = DATETIME(CURRENT_TIMESTAMP, 'localtime') WHERE code = 'USD'" % (USDTOD))
+			if row[0] == 'EUR_RUB__TOD' and row[1] is not None:
+				EURTOD = row[1]
+				cursor.execute("UPDATE ticker SET value = %f, dt = DATETIME(CURRENT_TIMESTAMP, 'localtime') WHERE code = 'EUR'" % (EURTOD))
+		update_prices(url_stocks)
+		update_prices(url_bonds_TQOB)
+		update_prices(url_bonds_TQCB)
+		cursor.execute("UPDATE subscribe SET refval = (SELECT value FROM ticker WHERE id = subscribe.ticker) WHERE type = 0 AND refval < (SELECT value FROM ticker WHERE id = subscribe.ticker)")
+		cursor.execute("UPDATE subscribe SET refval = (SELECT value FROM ticker WHERE id = subscribe.ticker) WHERE type = 1 AND refval > (SELECT value FROM ticker WHERE id = subscribe.ticker)")
+		conn.commit()
+		cursor.execute("SELECT uid, t.code, type, refval, t.value, ticker FROM subscribe s JOIN ticker t ON t.id = s.ticker WHERE type = 1 AND t.value > refval*1.0025 OR type = 0 AND t.value < refval*0.9975")
+		rows = cursor.fetchall()
+		for row in rows:
+			dir = "вырос \U0001F4C8" if row[2] else "упал \U0001F4C9"
+			dirn = "rise" if row[2] else "fall"
+			text = "%s %s %s с %.4f до %.4f. %s Продолжить наблюдение: /sub_%s_%s" % (random.choice(alarm_txt_prev), row[1], dir, row[3], row[4], random.choice(emoji), row[1].lower(), dirn)
+			updater.bot.send_message(chat_id=row[0], text=text)
+			cursor.execute("DELETE FROM subscribe WHERE uid = %d AND ticker = %d AND type = %d" % (row[0], row[5], row[2]))
+		conn.commit()
+	finally:
+		lock.release()
 	time.sleep(60)
